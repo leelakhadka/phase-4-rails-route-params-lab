fca43380af76046a25a89fa0a9622395f22593e9
leela khadka <khadkaleela3@gmail.com>
2022-10-11 15:11:50 -0700

    commit tree 17505b232f2ae9368479a2ee32f62433c602e29d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,14 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    students = { key1: "first_name", key2: "last_name" }
     render json: students
+  end
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
+
+
